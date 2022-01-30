040f5c92ea0699803939bfe4b19db07dc8fd9263
Ian <ianmcm.89@gmail.com>
2022-01-30 16:42:34 -0500

    commit tree e48e56b4e0e897af1d7ef24469557864c7cabaea

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    @a, @b, @c = [a, b, c].sort
+    if a <= 0 || a + b <= c then raise TriangleError
+    elsif a == c then :equilateral
+    elsif a == b || b == c then :isosceles
+    else :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
